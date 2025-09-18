972cdbc1841e1a1b882c578b84e63e9bb8f2036b
Prajakta Kulkarni <your.email@example.com>
2025-09-18 18:29:01 +0200

    Initial Commit

---
+++ Pick-a-Pal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pick-a-Pal
+//
+//  Created by Prajakta Kulkarni on 18/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Prajakta Kulkarni on 18/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
